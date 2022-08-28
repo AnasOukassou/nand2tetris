4abf4c79737a5f016a202ed0f93c218898b4f996
Anas OUKASSOU <anas@gmail.com>
2022-08-28 03:11:06 +0200

    Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,46 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(KB_LISTEN)
+    @24576
+    D=M
+    @FILLSCREEN_WHITE
+    D;JEQ
+
+(FILLSCREEN_BLACK)
+    @row
+    M=0
+    @color
+    M=-1
+    @FILLSCREEN_LOOP
+    0;JMP
+
+(FILLSCREEN_WHITE)
+    @row
+    M=0
+    @color
+    M=0
+
+(FILLSCREEN_LOOP)
+    @row
+    D=M
+    @8192
+    D=D-A
+    @KB_LISTEN
+    D;JGE
+    @row
+    D=M
+    @SCREEN
+    D=A+D
+    @R0
+    M=D
+    @color
+    D=M
+    @R0
+    A=M
+    M=D
+    @row
+    M=M+1
+    @FILLSCREEN_LOOP
+    0;JMP
